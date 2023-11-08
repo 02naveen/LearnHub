6769d36739deb258024ff336fbc53ac00d7b7bf0
Naveen <naveenkushwaha890@gmail.com>
2023-11-08 11:15:13 -0800

    ok

--- LearnHub/signupVC.swift
+++ LearnHub/signupVC.swift
@@ -8,12 +8,39 @@
 
 import UIKit
 
-class signupVC: UIViewController {
+class signupVC: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var f4: UITextField!
+    @IBOutlet weak var f3: UITextField!
+    @IBOutlet weak var f2: UITextField!
+    @IBOutlet weak var f1: UITextField!
+    @IBOutlet weak var create: UIButton!
+    
+    @IBAction func createbtn(_ sender: Any) {
+         let okhandler1 = {
+           (action:UIAlertAction)->Void in
+             self.performSegue(withIdentifier: "aba", sender: nil)
+        }
+        let alert2 = UIAlertController(title: "Account Created", message: "Click Ok for Login", preferredStyle: .alert)
+        alert2.addAction(UIAlertAction(title: "Ok", style: .default, handler: okhandler1));
+        self.present(alert2,animated: true,completion: nil)
+        
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        f1.resignFirstResponder()
+        f2.resignFirstResponder()
+        f3.resignFirstResponder()
+        f4.resignFirstResponder()
+        return true
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
     
     
